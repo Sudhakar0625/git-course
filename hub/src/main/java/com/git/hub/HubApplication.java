876360dbda898f3fe60a414cd2e7b11bf26616e8
sudhakar <srisudhakar56@gmail.com>
2025-07-02 23:07:20 +0530

    git--latest1

--- hub/src/main/java/com/git/hub/HubApplication.java
+++ hub/src/main/java/com/git/hub/HubApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HubApplication {
-
+	//GOOD to Know
 	public static void main(String[] args) {
 		SpringApplication.run(HubApplication.class, args);
 	}
